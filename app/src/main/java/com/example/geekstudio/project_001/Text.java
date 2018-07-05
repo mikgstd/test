1faae71835554664b2361752c290a681259ac219
Mikhail Ivanov <mikgstd@gmail.com>
2018-07-05 16:23:54 +0400

    Four commit

--- app/src/main/java/com/example/geekstudio/project_001/Text.java
+++ app/src/main/java/com/example/geekstudio/project_001/Text.java
@@ -4,5 +4,6 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Other text");
+        System.out.println("l");
     }
 }
