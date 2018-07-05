a82ace10cfecbebf23de746e0c5166d484b01131
Mikhail Ivanov <mikgstd@gmail.com>
2018-07-05 16:29:28 +0400

    Five commite

--- app/src/main/java/com/example/geekstudio/project_001/Text.java
+++ app/src/main/java/com/example/geekstudio/project_001/Text.java
@@ -2,7 +2,6 @@ package com.example.geekstudio.project_001;
 
 public class Text {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.println("Other text");
         System.out.println("l");
     }
