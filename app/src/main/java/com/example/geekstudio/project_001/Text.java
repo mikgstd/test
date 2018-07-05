d75eff5799825433e9e2c1e76d1b14e6253ad675
Mikhail Ivanov <mikgstd@gmail.com>
2018-07-05 14:30:49 +0400

    Three commit

--- app/src/main/java/com/example/geekstudio/project_001/Text.java
+++ app/src/main/java/com/example/geekstudio/project_001/Text.java
@@ -3,5 +3,6 @@ package com.example.geekstudio.project_001;
 public class Text {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Other text");
     }
 }
